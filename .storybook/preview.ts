f77a7ce2749aaa90a022d900b812cc54d1b6ad92
mnewen-a11y <mnewen@googlemail.com>
2026-02-12 20:52:43 +0100

    Add Storybook preview configuration

---
+++ .storybook/preview.ts
@@ -0,0 +1,36 @@
+import type { Preview } from '@storybook/react';
+import '../src/styles/globals.css';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#ffffff',
+        },
+        {
+          name: 'dark',
+          value: '#171717',
+        },
+        {
+          name: 'gray',
+          value: '#f5f5f5',
+        },
+      ],
+    },
+    docs: {
+      toc: true,
+    },
+  },
+};
+
+export default preview;
